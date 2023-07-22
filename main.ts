473f583b2a5fbb113fbf54164124e74976d92d21
joannadomagala <127305810+joannadomagala@users.noreply.github.com>
2023-07-23 01:01:32 +0200

    Aktualizacja pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,14 @@
-input.onPinPressed(TouchPin.P0, function () {
-    basic.showIcon(IconNames.Surprised)
-    if (light2 == 0) {
-        light2 = 1
-    } else {
-        light2 = 0
-    }
-})
-let light2 = 0
-basic.showIcon(IconNames.Happy)
+let duty = 0
 basic.forever(function () {
-    if (light2 == 1) {
-        pins.analogWritePin(AnalogPin.P2, pins.analogReadPin(AnalogPin.P1))
-    } else {
-        pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.showIcon(IconNames.Happy)
+    while (duty < 1023) {
+        pins.analogWritePin(AnalogPin.P0, duty)
+        duty = duty + 1
+        basic.pause(10)
+        while (duty > 1023) {
+            pins.analogWritePin(AnalogPin.P0, duty)
+            duty = duty - 1
+            basic.pause(10)
+        }
     }
 })
